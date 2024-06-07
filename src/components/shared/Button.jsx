a46a6ab8f25726ba0fd11fb1d90ce81032e15f9b
Kevin CT <kcayo6155@uta.edu.ec>
2024-06-06 23:45:27 -0500

    refactor: Cambia el tipo de animación en las Cards y el estilo de Boton por el tipo de version

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,9 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
-function ButtonG({ iconName, url, label, className }) {
+function ButtonG({ iconName, url, label, version }) {
   return (
-    <button className={className} onClick={() => (window.location.href = url)}>
+    <button
+      className={`${
+        version == "live"
+          ? "text-sm w-1/2 py-3 bg-gradient-to-r from-blue-500 to-cyan-500 rounded-full bg-gradient-hover px-5 mt-4"
+          : "flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500"
+      }`}
+      onClick={() => (window.location.href = url)}
+    >
       {iconName ? <FontAwesomeIcon icon={iconName} /> : null}
       {label}
     </button>
@@ -16,9 +23,5 @@ ButtonG.propTypes = {
   iconName: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
   url: PropTypes.string,
   label: PropTypes.string,
-  className: PropTypes.string,
-};
-
-ButtonG.defaultProps = {
-  className: "",
+  version: PropTypes.string,
 };
--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -6,9 +6,9 @@ function Card({ url_img, title, description, url_page, url_github, tools }) {
   return (
     <motion.div
       className="border  rounded-md p-5 flex-1 "
-      initial={{ opacity: 1, y: -25 }}
-      whileInView={{ opacity: 1, y: -5 }}
-      transition={{ duration: 5 }}
+      initial={{ opacity: 1, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeInOut" }}
     >
       <img src={url_img} alt="" width={200} className="w-full h-auto" />
       <h3 className="text-xl font-semibold mt-5">{title}</h3>
@@ -23,16 +23,8 @@ function Card({ url_img, title, description, url_page, url_github, tools }) {
       </div>
       <p className="text-gray-400">{description}</p>
       <div className="flex  mt-12 gap-1 ">
-        <ButtonG
-          className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700"
-          url={url_page}
-          label={"Live preview"}
-        />
-        <ButtonG
-          className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500"
-          url={url_github}
-          label={"Github"}
-        />
+        <ButtonG version="live" url={url_page} label={"Live preview"} />
+        <ButtonG version="code" url={url_github} label={"Github"} />
       </div>
     </motion.div>
   );
