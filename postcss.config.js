9cd659622eb863481415452aed09736e39c3f2f1
Kevin CT <kcayo6155@uta.edu.ec>
2025-02-26 00:31:09 -0500

    style: Integra libreria DaisyUI con tailwindcss mejora el diseño

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// En postcss.config.js
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [tailwindcss, autoprefixer],
+};
